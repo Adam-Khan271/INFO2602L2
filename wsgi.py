acc2d66fe482e43fed3742f86444ffcf9497c477
Adam-Khan271 <greenelmod@gmail.com>
2025-02-13 02:33:56 +0000

    modifications

--- wsgi.py
+++ wsgi.py
@@ -10,5 +10,21 @@ def initialize():
   db.init_app(app)
   db.create_all()
   bob = User('bob', 'bob@mail.com', 'bobpass')
-  print(bob)
+  db.session.add(bob)
+  db.session.commit()
   print('database intialized')
+
+@app.cli.command("get-user", help="Retrieves a User")
+@click.argument('username', default='bob')
+def get_user(username):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+    print(f'{username} not found!')
+    return
+  print(bob)
+
+@app.cli.command('get-users')
+def get_users():
+  # gets all objects of a model
+  users = User.query.all()
+  print(users)
